3ef5c45f6f81f7bbad471c33a52bcef93ead088d
Typonote <kimradium@naver.com>
2021-10-29 15:24:56 +0900

    #2 kiosk 메인 화면 반응형 코드 삽입

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import TouchAppIcon from '@material-ui/icons/TouchApp';
 import React from 'react'
 import meal from '../assets/images/meal.png'
+import touch from '../assets/images/touch.svg'
 
 const Home = (props) => {
 
@@ -11,23 +12,22 @@ const Home = (props) => {
             className='w-full h-screen '
             onClick={() => props.history.push('/choose')}
             >
-                <div className = 'w-full h-screen text-white bg-red-300  p-5 cursor-pointer'>         
+                <div className = 'w-full h-screen text-white bg-yellow-300  p-5 cursor-pointer'>         
                     <div className = 'flex flex-col overflow-auto text-white items-center my-5'>      
-                        <p className='font-mono font-black text-8xl text-center my-12'>
+                        <p className='font-mono font-black text-4xl text-center my-12 sm:text-5xl md:text-8xl'>
                         Le Cordon Bleu
                         </p>
-                        <p className='font-mono font-black text-5xl text-center mb-2'>     
+                        <p className='font-mono font-black text-3xl sm:text-4xl md:text-5xl text-center mb-2'>     
                         주문을 원하시면 
                         </p>
-                        <p className='font-mono font-black text-5xl text-center mb-10'>     
+                        <p className='font-mono font-black text-3xl sm:text-4xl md:text-5xl text-center mb-10'>     
                         화면을 클릭해주세요.
                         </p>
-                        <img class="w-6/12 rounded  object-cover object-center mb-10" src={meal} alt="" />
+                        <img class="w-10/12 sm:w-6/12 rounded  object-cover object-center mb-10" src={meal} alt="" />
                         
                     </div>
-                    <button className= 'w-full flex justify-center items-center text-center bg-green-300  hover:bg-green-200 mt-10'> 
-                    <TouchAppIcon className='my-5' style={{width:'5rem',height:'5rem'}}/>
-                        <p className='font-mono font-black text-5xl text-center'> 
+                    <button className= 'w-full flex justify-center items-center text-center rounded-md bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500  hover:bg-green-200 mt-10'> 
+                        <p className='font-mono font-black text-3xl sm:text-4xl md:text-5xl  text-center py-5'> 
                         주문시작
                         </p>
                     </button>
